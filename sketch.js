dff6881003bd0fa810c60102fec7f36f1166a53e
olibraun <oliverbraun89@gmail.com>
2018-04-04 14:27:21 +0200

    feat: First draft of QuickHull method.

--- sketch.js
+++ sketch.js
@@ -40,3 +40,54 @@ function draw() {
   });
   endShape();
 }
+
+function QuickHull(S) {
+  // Find convex hull from the set S of n points
+
+  // Find left and right most points, say A & B, and add A & B to convex hull
+
+  let minX = width;
+  let maxX = 0;
+  let minXIndex = 0;
+  let maxXIndex = 0;
+  for(let i = 0; i < S.length; i++) {
+    if(S[i].x < minX) {
+      minX = S[i].x;
+      minXIndex = i;
+    }
+    if(S[i].x > maxX) {
+      maxX = S[i].x;
+      maxXIndex = i;
+    }
+  }
+
+  // A and B have been found by this procedure.
+  const A = S[minXIndex];
+  const B = S[maxXIndex];
+  convexHull.push(A);
+  convexHull.push(B);
+
+  // Segment AB divides the remaining (n-2) points into 2 groups S1 and S2 
+  // where S1 are points in S that are on the right side of the oriented line from A to B, 
+  // and S2 are points in S that are on the right side of the oriented line from B to A
+  let S1 = [];
+  let S2 = [];
+  for(let i = 0; i < S.length; i++) {
+    if(S[i] != A && S[i] != B) {
+      const AB = A.sub(B);
+      const scalarprod = S[i].x*AB.x + S[i].y*AB.y;
+      if(scalarprod >= 0) {
+        S1.push(S[i]);
+      } else {
+        S2.push(S[i]);
+      }
+    }
+  }
+
+  FindHull(S1, A, B);
+  FindHull(S2, B, A);
+}
+
+function FindHull(Sk, P, Q) {
+  return;
+}
